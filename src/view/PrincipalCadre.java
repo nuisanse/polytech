b56721e1230a00ec539207b0dcdd053d050d2328
Mohamed Reda Naja <mohamed-reda.naja@ensimag.grenoble-inp.fr>
2017-04-12 17:44:05 +0200

    mmm

--- src/view/PrincipalCadre.java
+++ src/view/PrincipalCadre.java
@@ -1,14 +1,17 @@
 package view;
 
+import java.util.ArrayList;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import IHM.Aide;
+import IHM.Carte;
 import IHM.Catalogue;
+import IHM.FinJeu;
 import IHM.Jeu;
 import IHM.Jeu2;
 import IHM.Jeu3;
-import IHM.FinJeu;
 import IHM.Menu;
 import IHM.Tableauscore;
 
@@ -29,11 +32,49 @@ public class PrincipalCadre extends JFrame {
 	private Jeu3 jeu3 = null;
 	private FinJeu finjeu = null;
 	
+	public ArrayList<Carte> liste1 = new ArrayList<Carte>(); //theme 1
+	public ArrayList<Carte> liste2 = new ArrayList<Carte>(); //theme 2
+	public ArrayList<Carte> liste3 = new ArrayList<Carte>(); //theme 3
+	public ArrayList<String> dicoAr = new ArrayList<String>(); // arabe
+	public ArrayList<String> dicoFr = new ArrayList<String>(); // français
+	public ArrayList<String> dicoAng = new ArrayList<String>(); // anglais
+	public ArrayList<String> dicoAl = new ArrayList<String>(); // allemand
+	
+	public ArrayList<Carte> listeTheme(String theme)
+	{
+		if(theme.equals("Animaux"))
+			return liste1;
+		else if(theme.equals("Nourriture"))
+			return liste2;
+		else
+			return liste3;
+	}
+	
+	public ArrayList<String> listeDico(String langue)
+	{
+		if(langue.equals("Arabe"))
+			return dicoAr;
+		else if(langue.equals("Anglais"))
+			return dicoAng;
+		else if(langue.equals("Allemand"))
+			return dicoAng;
+		else
+			return dicoFr;
+	}
 	
 	public PrincipalCadre()
 	{
 		super();
+		/* Remplissage des tableaux de cartes */
+		liste1.add(new Carte("abeille-Ar-.png","abeille-fr-.png","abeille-En-.png","abeille-Germ-.png","abeille.png","nomFr","nomAng","allemand","arabe"));
+		/* fin du remplissage */
 		initialize();
+	}
+	
+	public void creationliste()
+	{
+		
+		
 	}
 
 	
